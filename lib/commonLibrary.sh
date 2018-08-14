c5b0dadcda082189ba870a7d2691aa61e2a802d9
divya <divya_bhushan@hotmail.com>
2018-08-14 11:54:12 +0530

    Adding library functions

--- lib/commonLibrary.sh
+++ lib/commonLibrary.sh
@@ -2,3 +2,15 @@
 
 #author: divya
 #aim: This is a common library file.
+
+set_variables(){
+DIR="."
+}
+
+exit_status(){
+if[$? -ne 0];then
+	echo "Process failed !!! exiting the program..."
+	exit 1;
+fi
+}
+
